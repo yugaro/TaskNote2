fb8084535613bf1cdf4499648aa5ba3b741ecb93
yugaro <yugaro.yugaro@gmail.com>
2021-11-24 15:27:21 +0900

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import tw from 'tailwind-rn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={tw('flex-1 bg-gray-300 justify-center items-center')}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
